cb06945b466db3a50d6db56ee6a49d54e74e4c5d
Kostadina Geceska <kostadinag@nextsense.com>
2024-07-09 22:19:42 +0200

    initial

---
+++ Bossy.xcframework/ios-arm64/Bossy.framework/Headers/Bossy.h
@@ -0,0 +1,18 @@
+//
+//  Bossy.h
+//  Bossy
+//
+//  Created by Kostadina Gecevska on 9.7.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Bossy.
+FOUNDATION_EXPORT double BossyVersionNumber;
+
+//! Project version string for Bossy.
+FOUNDATION_EXPORT const unsigned char BossyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bossy/PublicHeader.h>
+
+
